16874e66d5a3d37743d006d8ee7b4a5ca7610d57
amar1452001 <amarjotsingh5555@gmail.com>
2023-02-22 07:23:09 +0530

    adding files

--- AMAR-MODULE_FILE/main.tf
+++ AMAR-MODULE_FILE/main.tf
@@ -2,28 +2,29 @@
 module "AMAR-EC2" {
     source = "/home/amar/Documents/TERRAFORM/SPRINT 4/MODULE_IMPLEMENT_1/AMAR-EC2"
     
-    ami_id = "ami-0f8ca728008ff5af4"
+    ami_id = var.ami_id_EC2
     
-    inst_type = "t2.micro"
+    inst_type = var.instance_type_EC2
     
-    tag_name = "Amarjot Singh"
+    tag_name = var.tag_name_value
     
-    tag_owner = "amarjot.singh@cloudeq.com"
+    tag_owner = var.tag_owner_value
     
-    tag_purpose = "Training"
+    tag_purpose = var.tag_purpose_value
   
 }
 
 module "AMAR-S3" {
+
     source = "/home/amar/Documents/TERRAFORM/SPRINT 4/MODULE_IMPLEMENT_1/AMAR-S3"
 
-    bucket_name = "amarS3bucket"
+    bucket_name = var.bucket_name_S3
 
-    tag_name = "Amarjot Singh"
+    tag_name = var.tag_name_value
     
-    tag_owner = "amarjot.singh@cloudeq.com"
+    tag_owner = var.tag_owner_value
     
-    tag_purpose = "Training"
+    tag_purpose = var.tag_owner_value
 
     
 }
